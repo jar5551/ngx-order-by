4ac369bc5c8852efb227f72c6a431e83f35eed4f
Jarosław Frydrych <jarek.frydrych@gmail.com>
2017-11-06 19:46:11 +0100

    v1

--- src/app/ngx-order-by/ngx-order-by.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NgxOrderByPipe } from './ngx-order-by.pipe';
-
-describe('NgxOrderByPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NgxOrderByPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
